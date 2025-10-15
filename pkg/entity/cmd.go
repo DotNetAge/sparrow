9844e39d0df1777028e3400ce789cae390cdf9d1
@MSCSharp <csharp2002@hotmail.com>
2025-10-15 15:42:51 +0800

    修复Command与Event中添加了多余的AggregateID的问题.

--- pkg/entity/cmd.go
+++ pkg/entity/cmd.go
@@ -5,9 +5,13 @@ type Command interface {
 	GetAggregateID() string
 }
 
+// BaseCommand 基础命令结构体，提供标准字段和方法
+// 所有命令都应该嵌入这个结构体来实现通用的Command接口方法
+// NOTES: 一切的Command都不可以重新实现GetAggregateID()方法，以及重新定义 AggregateID 字段,否则会引发系统性异常。
+// 除非所有命令处理机制都重新实现，系统会默认从JSON或HTTP的Body中将id转换成AggregateID字段。
 type BaseCommand struct {
 	CommandID   string `json:"command_id" form:"command_id"`
-	AggregateID string `json:"aggregate_id" form:"aggregate_id"`
+	AggregateID string `json:"id" form:"id"`
 }
 
 func (c *BaseCommand) GetCommandID() string {
--- pkg/entity/domain_base_event.go
+++ pkg/entity/domain_base_event.go
@@ -2,17 +2,20 @@ package entity
 
 import (
 	"time"
+
 	"github.com/google/uuid"
 )
 
 // BaseEvent 基础领域事件结构体，实现DomainEvent接口
+// NOTES: 一切的BaseEvent都不可以重新实现GetAggregateID()方法，以及重新定义 AggregateID 字段,否则会引发系统性异常。
+// 除非所有事件处理机制都重新实现，系统会默认从JSON或HTTP的Body中将id转换成AggregateID字段。
 type BaseEvent struct {
-	Id            string    `json:"id"`
-	AggregateID   string    `json:"aggregate_id"`
-	EventType     string    `json:"event_type"`
-	AggregateType string    `json:"aggregate_type"`
-	Timestamp     time.Time `json:"timestamp"`
-	Version       int       `json:"version"`
+	Id            string      `json:"id"`
+	AggregateID   string      `json:"aggregate_id"`
+	EventType     string      `json:"event_type"`
+	AggregateType string      `json:"aggregate_type"`
+	Timestamp     time.Time   `json:"timestamp"`
+	Version       int         `json:"version"`
 	Payload       interface{} `json:"payload,omitempty"` // 事件负载数据
 }
 
