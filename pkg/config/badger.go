a4b0578702f5881a298ae354046b523317371c52
@MSCSharp <csharp2002@hotmail.com>
2025-10-15 22:48:11 +0800

    修正Badger

--- pkg/config/badger.go
+++ pkg/config/badger.go
@@ -5,7 +5,7 @@ type BadgerConfig struct {
 	EventStoreDir  string `mapstructure:"es_dir"`
 	ValueThreshold int64  `mapstructure:"value_threshold"`
 	NumCompactors  int    `mapstructure:"num_compactors"`
-	InMemory       bool   `mapstructure:"in_memory"`
-	MemTableSize   int64  `mapstructure:"mem_table_size"`
-	MaxTableSize   int64  `mapstructure:"max_table_size"`
+	// InMemory       bool   `mapstructure:"in_memory"`
+	// MemTableSize   int64  `mapstructure:"mem_table_size"`
+	// MaxTableSize   int64  `mapstructure:"max_table_size"`
 }
