c946397ed98748206a1982d226184b771b4832cc
@MSCSharp <csharp2002@hotmail.com>
2025-10-17 19:05:04 +0800

    增加http JSON结果集

--- pkg/http/results.go
+++ pkg/http/results.go
@@ -3,12 +3,17 @@ package http
 import (
 	h "net/http"
 
+	"github.com/DotNetAge/sparrow/pkg/entity"
 	"github.com/gin-gonic/gin"
 )
 
 // ERROR 通用错误响应
 func ERROR(c *gin.Context, statusCode int, message string) {
 	c.JSON(h.StatusOK, gin.H{"code": statusCode, "msg": message})
+}
+
+func FAIL(c *gin.Context, message string) {
+	c.JSON(h.StatusOK, gin.H{"code": h.StatusInternalServerError, "msg": message})
 }
 
 // BAD_REQ 400 错误响应
@@ -37,13 +42,13 @@ func DATA(c *gin.Context, data interface{}) {
 }
 
 // PAGE_DATA 200 分页数据成功响应
-func PAGE_DATA(c *gin.Context, data interface{}, total int, page, size int) {
+func RESULT[T any](c *gin.Context, result *entity.PaginatedResult[T]) {
 	c.JSON(h.StatusOK, gin.H{
 		"code":  0,
 		"msg":   "success",
-		"data":  data,
-		"total": total,
-		"page":  page,
-		"size":  size,
+		"data":  result.Data,
+		"total": result.Total,
+		"page":  result.Page,
+		"size":  result.Size,
 	})
 }
